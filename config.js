bd64b97d392931c846685425efe9baa8363b2ddf
MyNameIsTrez <welfje@gmail.com>
2020-04-19 01:03:41 +0200

    .gitignore is now working

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	consumer_key: '7AORDbSuKwg9LU5a9q69toi02',
-	consumer_secret: 'hLIz9ObKkUNEnQvdPyHNmjkezYldsD4zq3mXAxTMYrtkHDo71O',
-	access_token: '1250532169275052032-jCZnayJXNSi07pA58pxt6ZLUA6jca4',
-	access_token_secret: 'G0pqOVkDYiamZpCNUZTd2LwtWfXm300nTDozUSnIPZL4m',
-	timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
-	strictSSL: true,     // optional - requires SSL certificates to be valid.
-}
