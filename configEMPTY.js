215cb2ebbc5c7763035f74d38a52a185bb8bebc5
MyNameIsTrez <welfje@gmail.com>
2020-04-19 01:11:41 +0200

    Added instructions in configEMPTY.js, removed temp1.js

--- configEMPTY.js
+++ configEMPTY.js
@@ -1,8 +1,12 @@
+// Rename this file to 'config.js' when you're done!
+
 module.exports = {
+	// fill in these four keys using your Twitter API app by heading to 'Keys and tokens':
 	consumer_key: '',
 	consumer_secret: '',
 	access_token: '',
 	access_token_secret: '',
+
 	timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
 	strictSSL: true,     // optional - requires SSL certificates to be valid.
 }
